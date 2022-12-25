c121bb118cf727ebf79feba66bae52cad1593b32
JoMa388 <jonathanma388@gmail.com>
2022-12-25 16:32:23 -0500

    Added PHPFunctions

---
+++ PHPFunctions/Vowelremoval.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="get">
+        <label for="input"> Enter a String</label>
+        <input type="text" name="input"> 
+        <button>Submit</button>
+    </form>
+    <?php
+        /*
+        Takes string input and prints out word without vowels
+         */
+        $input= $_GET['input'];
+    function VowelRemove($string) {
+        $vowels = array('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U');
+        for ($i = 0; $i < strlen($string); $i++) {
+            if (in_array($string[$i], $vowels)) {
+                continue;
+            }
+            echo $string[$i];
+        }
+    }
+    if($input!=null) {
+        VowelRemove($input);
+    }
+    ?>
+</body>
+</html>
---
+++ PHPFunctions/printTest.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="get"> 
+        
+        <button name="print" value="print">Print Test</button>
+    </form>
+    <?php
+    /* 
+    Prints 1 to 100, Fizz instead for multiples of 3, 
+    Buzz instead for multiples of 5 
+     */
+$button = $_GET['print'];
+    function Printtest ($button) {
+        echo "<br>"."<br>";
+        for($i=0;$i<101;$i++) {
+            if($i%3==0) {
+                echo "Fizz"."<br>";
+                continue;
+            }
+            if($i%5==0) {
+                echo "Buzz"."<br>";
+                continue;
+            }
+            echo $i."<br>";
+        }
+    }
+if ($button != null) {
+    Printtest($button);
+}
+    ?>
+</body>
+</html>
