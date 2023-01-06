06b900652def5e85b9024fa7a187fc26124bc940
JoMa388 <jonathanma388@gmail.com>
2023-01-06 17:04:05 -0500

    Updated Contact Form with database

--- ContactForm/script.js
+++ ContactForm/script.js
@@ -6,40 +6,40 @@ const phone=document.querySelector("#phoneNumber");
 const messageInput=document.querySelector("#message");
 const success=document.querySelector("#success");
 const errorNodes=document.querySelectorAll(".error");
-
 // Validate data
 function validateForm() {
     clearMessages();
-
     if(firstInput.value.length<1) {
         errorNodes[0].innerText= "Name cannot be blank";
         firstInput.classList.add("error-border");
+        return false;
     }
     if(lastInput.value.length<1) {
         errorNodes[1].innerText= "Name cannot be blank";
         lastInput.classList.add("error-border");
+        return false;
     }
     if(address.value.length<1) {
         errorNodes[2].innerText= "Please enter a address";
         address.classList.add("error-border");
+        return false;
     }
     if(stateSelection()=="Null") {
         errorNodes[3].innerText= "Please Select a State";
+        return false;
     }
     if(!validatePhoneNumber(phone.value)) {
         errorNodes[4].innerText= "Please enter a phone number";
         phone.classList.add("error-border");
+        return false;
+
     }
     if(messageInput.value.length<1) {
         errorNodes[5].innerText= "Please enter a message";
         messageInput.classList.add("error-border");
-        var errorFlag = true;
+        return false;
     }
-
-    if(!errorFlag) {
-        success.innerText= "Submitted";
-    }
-
+    return true;
 }
 
 // Clear error/ Success messages
@@ -65,3 +65,7 @@ function stateSelection () {
     stateValue=document.getElementById("state").value;
     return stateValue;
 }
+function mySubmitFunction(e) {
+    e.preventDefault();
+    return false;
+  }
