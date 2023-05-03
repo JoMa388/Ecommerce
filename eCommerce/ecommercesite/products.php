9458469460b622b03fc8d78256fbc5071967a811
JoMa388 <jonathanma388@gmail.com>
2023-05-03 19:07:41 -0400

    products 4 item per row

--- eCommerce/ecommercesite/products.php
+++ eCommerce/ecommercesite/products.php
@@ -14,26 +14,29 @@
     include_once 'siteIncludes/siteHeader.php';
     include_once '../uploadsite/includes/db_conn.php';
     ?>
-    <div class="container-xl mt-5 d-flex flex-wrap justify-content-between">
-        <?php
-        $sql = "SELECT * FROM product;";
-        $result = mysqli_query($conn, $sql);
-        while ($row = mysqli_fetch_assoc($result)) {
-            echo <<<product
-                <div class="card mb-4 ">
-                    <div class="card-body text-center">
-                        <img src="../bootstrapmorph/undraw_cat_s1wg.svg" alt="cat" class="image-thumbnail border-1" width="200">
-                        <h5 class="card-title">$row[pname]</h5>
-                        <p class="card-text">$row[pdesc]</p>
-                        <p class="card-text">$row[price]</p>
-                        <button type="submit" class="btn btn-primary m-auto">Add to Cart</button>
-                    </div>
-
+    <div class="container-xl mt-5 d-flex">
+        <div class="row">
+            <?php
+            $sql = "SELECT * FROM product;";
+            $result = mysqli_query($conn, $sql);
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo <<<product
+                <div class="col-sm-12 col-md-4 col-lg-3">
+                    <div class="card mb-4 me-3">
+                        <div class="card-body text-center">
+                            <img src="../bootstrapmorph/undraw_cat_s1wg.svg" alt="cat" class="image-thumbnail img-fluid mb-3">
+                            <h5 class="card-title">$row[pname]</h5>
+                            <p class="card-text">$row[pdesc]</p>
+                            <p class="card-text">$row[price]</p>
+                            <button type="submit" class="btn btn-primary m-auto">Add to Cart</button>
+                        </div>
+                </div>
             </div>
 product;
-        }
-        ?>
+            }
+            ?>
 
+        </div>
     </div>
 </body>
 <script src="../bootstrapmorph/js/bootstrap.js"></script>
